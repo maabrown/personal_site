4de434bdad0735174e81abd4ba34b27ba2c3e150
Mark Brown II <mbrownii@doc.gov>
2016-09-21 20:07:22 -0400

    Finish Gulp file setup - need to add sourcemaps

--- gulpfile.js
+++ gulpfile.js
@@ -3,10 +3,12 @@ var sass = require('gulp-sass');
 var deploy = require('gulp-gh-pages');
 var cred = require('./credentials/credentials.js');
 var browserSync = require('browser-sync').create();
-
-gulp.task('hello', function() {
-	console.log('Hello Zell');
-})
+var useref = require('gulp-useref');
+var gulpIf = require('gulp-if');
+var cleanCSS = require('gulp-clean-css');
+var uglify = require('gulp-uglify');
+var del = require('del');
+var runSequence = require('run-sequence');
 
 gulp.task('sass', function() {
 	return gulp.src('app/sass/**/*.scss')
@@ -15,13 +17,13 @@ gulp.task('sass', function() {
 		.pipe(browserSync.reload({
 			stream: true
 		}))
-})
+});
 
 gulp.task('watch', ['browserSync', 'sass'], function() {
 	gulp.watch('app/sass/**/*.scss', ['sass']);
 	gulp.watch('app/*.html', browserSync.reload);
 	gulp.watch('app/js/**/*.js', browserSync.reload);
-})
+});
 
 gulp.task('browserSync', function() {
 	browserSync.init({
@@ -29,11 +31,36 @@ gulp.task('browserSync', function() {
 			baseDir: 'app'
 		}
 	})
-})
+});
 
 gulp.task('deploy', function() {
 	return gulp.src('dist/**/*')
 		.pipe(deploy({
 			"remoteUrl": "https://" + cred.username + ":" + cred.personalToken + "@github.com/maabrown/personal_site.git"
 		}))
+});
+
+gulp.task('useref', function() {
+	return gulp.src('app/*.html')
+		.pipe(useref())
+		.pipe(gulpIf('*.js', uglify()))
+		.pipe(gulpIf('*.css', cleanCSS()))
+		.pipe(gulp.dest('dist'))
+});
+
+gulp.task('clean:dist', function() {
+	return del.sync('dist');
+});
+
+gulp.task('build', function(callback) {
+	runSequence('clean:dist',
+		['sass', 'useref'],
+		callback
+	)
+});
+
+gulp.task('default', function(callback) {
+	runSequence(['sass', 'browserSync', 'watch'],
+		callback
+	)
 });
